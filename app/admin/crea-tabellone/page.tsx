3d31070962142987aaa2246c5274ac3ae365b980
pepi88-88 <pepi88fly@gmail.com>
2026-09-05 17:21:20 +0200

    Update page.tsx

--- app/admin/crea-tabellone/page.tsx
+++ app/admin/crea-tabellone/page.tsx
@@ -1768,7 +1768,7 @@ const winnerTop  = final.top + SE_FINAL_TUNE.boxDY
   style={{ 
     width: CARD_W, 
     height: CARD_H, 
-    left: final.left + CARD_W + 120, 
+    left: final.left + CARD_W -40, 
     top: final.top + CARD_H + 80 
   }}
 >
