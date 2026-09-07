04d51519f63b2ebf49f6a5eac3cdaec04b351fe2
pepi88-88 <pepi88fly@gmail.com>
2026-09-07 16:27:58 +0200

    Update BracketCanvas.tsx

--- components/BracketCanvas.tsx
+++ components/BracketCanvas.tsx
@@ -842,7 +842,7 @@ if (bracket.type === 'PSE' && pseLayout) {
   return (
     <path
       key={`pse-${i}`}
-      d={`M ${16 + CARD_W} ${y} H ${target.left - 80}`}
+     d={`M ${16 + CARD_W} ${y} H ${target.left + CARD_W - 80}`}
       stroke={bracket.color}
       strokeWidth={3}
       fill="none"
