b3556f6b2f2db2fa4e016f12a852257e72246399
pepi88-88 <pepi88fly@gmail.com>
2026-09-07 23:24:25 +0200

    Update BracketCanvas.tsx

--- components/BracketCanvas.tsx
+++ components/BracketCanvas.tsx
@@ -935,10 +935,7 @@ d={`M ${CARD_W} ${y} H ${target.left -95}`}
 {/* BOX P */}
 {se.preNodes?.map((p,i)=>{
 
-const m = {
-  A: bracket.slots?.[i*2] ?? '-',
-  B: bracket.slots?.[i*2+1] ?? '-'
-}
+const m = bracket.r1?.[i] ?? {A:'-',B:'-'}
 return (
   <div
     key={`pre-${i}`}
